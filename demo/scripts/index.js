e4a98477ea1ec32b648683cc374db93cd5bb3514
vir16 <83710312+vir16@users.noreply.github.com>
2021-08-14 18:58:49 +0530

    test

---
+++ demo/scripts/index.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 30;
+console.log(a + b);
+if (a + b > 30) console.log("greater than 30");
+
+let n=23
+let isPrime=true
+for(i=2;i<=n-1;i++){
+    if(n%i==0){
+        isPrime=false
+    }
+}
+
+if(isPrime)
+
+let car={
+    color: "red",
+    model: "sedan"
+}
+
+document.getElementById("heading").addEventListener("mouseenter", () => {
+    document.getElementById("heading").style.color = "red";
+  });
+  
